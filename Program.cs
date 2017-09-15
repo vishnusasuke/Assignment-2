cf26871d25c31044366518dab7d85dd8d1fa33a9
vishnusasuke <vishnusasuke@gmail.com>
2017-09-15 00:38:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Idisposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             BaseClass bc = new BaseClass();
+            //Console.WriteLine("Enter the Student ID : ");
+            //int ID = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the Student Name : ");
+            //int Name = Convert.ToInt32(Console.ReadLine());
+            Program e = new Program();
+            bc.Dispose();
+            Console.WriteLine("resource cleared and next statement is executed");
+            Console.ReadKey();          
+            
+        }
+    }
+}
